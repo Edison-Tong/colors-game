bbdcee2a28669a183bb343277cf5b3b82f8e6a4f
Edison-Tong <edisonjtong@gmail.com>
2025-04-29 17:57:46 -0600

    init

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js or your screen file
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PagerView from "react-native-pager-view";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PagerView style={styles.pagerView} initialPage={0}>
+      <View key="1" style={styles.page}>
+        <Text style={styles.text}>Page 1: Info A</Text>
+      </View>
+      <View key="2" style={styles.page}>
+        <Text style={styles.text}>Page 2: Info B</Text>
+      </View>
+    </PagerView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pagerView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "grey",
+  },
+  text: {
+    fontSize: 24,
   },
 });
